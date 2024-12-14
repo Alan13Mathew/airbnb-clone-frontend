d03bba55b43b2758a5c2bf02b5cac503c852d37f
Alan Mathew <alanmathew0891@gmail.com>
2024-12-14 13:13:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+  return <>
+    <h1 class="text-green-500 text-4xl font-bold">
+        Geeksforgeeks
+    </h1>
+  </>
+}
+
+export default App;
